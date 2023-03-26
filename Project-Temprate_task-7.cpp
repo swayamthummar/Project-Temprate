6ea8bc1d54f54f2c82d257e04637eafe078d05c5
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-03-26 14:38:51 +0530

    Update Project-Temprate_task-7.cpp

--- Project-Temprate_task-7.cpp
+++ Project-Temprate_task-7.cpp
@@ -2,30 +2,31 @@
 main()
 {
 	int n,i,ans=0;
-	 printf("Enter a positive integer: ");
-    scanf("%d", &n);
+	
+	 printf("Enter a positive integer->");
+         scanf("%d",&n);
     
     if (n==0 && n==1) 
 	{
-        printf("%d is not a prime number.", n);
-    }
-    else 
+        printf("%d is not a prime number.",n);
+        }
+       else 
 	{
         for (i=2;i<=n/2;i++) 
-		{
-            if (n%i==0) 
-			{
-                ans = 1;
+	{
+            if (n%i==0) 		
+	{
+                ans=1;
                 break;
-            }
+        }
         }
         if (ans == 0) 
-		{
-            printf("%d is a prime number.", n);
+	{
+            printf("%d is a prime number.",n);
         }
         else 
-		{
-            printf("%d is not a prime number.", n);
+	{
+            printf("%d is not a prime number.",n);
         }
     }
 }
